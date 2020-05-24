b19b4ddafcc7103e2a2b783e7dea3f17c297ab32
Levon T <itfibonacc@live.com>
2020-05-24 19:37:38 -0400

    added comments to the is_square.js file

--- is_square.js
+++ is_square.js
@@ -1,4 +1,6 @@
 // tests whether a given input is a perfect square
+// the function checks whether after taking the square root of the 
+// input, if it is an integer or not.
 
 var isSquare = function (n) {
     return Math.sqrt(n) % 1 === 0;
